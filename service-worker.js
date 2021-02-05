549e8d0232d88127eb037e24cb6ef9bdc4022620
DhanapalanDP <DhanapalanDP@users.noreply.github.com>
2021-02-05 10:58:07 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/DhanapalanPortfolio/precache-manifest.54e6e27291fedcea2d743d8b039c4f68.js"
+  "/portfolio/precache-manifest.33b5cc2c12096cc1a3a4aaa5715a01fc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/DhanapalanPortfolio/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
