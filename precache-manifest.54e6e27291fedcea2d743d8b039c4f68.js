dd8a170ac73c7ea089d26f39932e0e3a0b1d5eaa
DhanapalanDP <DhanapalanDP@users.noreply.github.com>
2021-02-05 10:38:11 +0530

    Updates

---
+++ precache-manifest.54e6e27291fedcea2d743d8b039c4f68.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "aeea70bceb5ba8328efd",
+    "url": "/DhanapalanPortfolio/static/css/main.4e0cb7d5.chunk.css"
+  },
+  {
+    "revision": "aeea70bceb5ba8328efd",
+    "url": "/DhanapalanPortfolio/static/js/main.dbb7455f.chunk.js"
+  },
+  {
+    "revision": "6e30ddd7c2bfe00f881b",
+    "url": "/DhanapalanPortfolio/static/js/runtime~main.cd0b3bac.js"
+  },
+  {
+    "revision": "47ce237dcbc20856e1fd",
+    "url": "/DhanapalanPortfolio/static/js/2.be624c06.chunk.js"
+  },
+  {
+    "revision": "49fccd5ce66e721d9809",
+    "url": "/DhanapalanPortfolio/static/js/3.ab19b920.chunk.js"
+  },
+  {
+    "revision": "aeedd51d9dba9338b5de806dd8fcde7e",
+    "url": "/DhanapalanPortfolio/static/media/Dp1.aeedd51d.png"
+  },
+  {
+    "revision": "006fb6bd94e71f42ee004817baca0cdf",
+    "url": "/DhanapalanPortfolio/static/media/background.006fb6bd.png"
+  },
+  {
+    "revision": "2f93df00b061c7af6d88f0c997462c03",
+    "url": "/DhanapalanPortfolio/static/media/Tik.2f93df00.PNG"
+  },
+  {
+    "revision": "bca9c7b678b009bf4d9dcd3fe61364e2",
+    "url": "/DhanapalanPortfolio/static/media/clock.bca9c7b6.png"
+  },
+  {
+    "revision": "c8f139a62bccd7ce5d199684394583c6",
+    "url": "/DhanapalanPortfolio/static/media/game.c8f139a6.png"
+  },
+  {
+    "revision": "2892eb5ff91f5d2e0d2b1f884a6bfb1f",
+    "url": "/DhanapalanPortfolio/static/media/expense.2892eb5f.png"
+  },
+  {
+    "revision": "aa2db292c216596e37ffdf4af7da0fcf",
+    "url": "/DhanapalanPortfolio/static/media/Calculator.aa2db292.png"
+  },
+  {
+    "revision": "53a322d78126324f0df2b63098f06433",
+    "url": "/DhanapalanPortfolio/static/media/todo.53a322d7.png"
+  },
+  {
+    "revision": "492accdf7c59902246a2a6b5695e8144",
+    "url": "/DhanapalanPortfolio/index.html"
+  }
+];
